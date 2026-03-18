9e589a49d4d40f61e8fed02872687ea6830faa7b
MR.MAG1C <80677728+MRxMAG1C@users.noreply.github.com>
2026-03-18 15:46:05 +0100

    Delete src directory

--- src/Categories.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserConfigSaver;
-
-public static class Categories
-{
-    public const int Hud = 1;
-    public const int Viewmodel = 2;
-    public const int Crosshair = 3;
-    public const int Mouse = 4;
-}
